3ed0fcff24e9aba8fd75047fd705357d640a6cd2
Pankaj Kumar <83492104+hack2ai@users.noreply.github.com>
2026-09-13 16:14:10 +0530

    Move ring storage to heap

--- include/ring_buffer.hpp
+++ include/ring_buffer.hpp
@@ -1,15 +1,18 @@
 #pragma once
 
 #include "packet.hpp"
-#include <array>
 #include <atomic>
 #include <cstddef>
 #include <cstdint>
+#include <memory>
 
 namespace telemetry {
 
 class SpscRingBuffer {
 public:
+    SpscRingBuffer()
+        : buffer_(std::make_unique<Frame[]>(kRingBufferCapacity)) {}
+
     bool try_push(std::uint64_t sequence,
                   std::uint16_t source,
                   std::uint16_t destination,
@@ -21,7 +24,7 @@ public:
     constexpr std::size_t capacity() const noexcept { return kUsableQueueCapacity; }
 
 private:
-    alignas(64) std::array<Frame, kRingBufferCapacity> buffer_{};
+    std::unique_ptr<Frame[]> buffer_;
     alignas(64) std::atomic<std::size_t> write_{0};
     alignas(64) std::atomic<std::size_t> read_{0};
 };
